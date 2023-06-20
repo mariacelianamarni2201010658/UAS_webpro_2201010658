028d57f027b045ff4415455b2b12970c9c768e17
mariacelianamarni2201010658 <127914115+mariacelianamarni2201010658@users.noreply.github.com>
2023-06-20 19:08:36 +0700

    Add files via upload

---
+++ UAS_webpro_2201010658/web projek.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<php lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+ 
+<div class="bg-warning p-4 bg-primary text-white text-center">
+  <h1>wellcome too </h1>
+  <p>hotel AYANA labuan bajo!</p> 
+  <td><img src="ayana 5.jpg" width="1000" height="400"></td>
+  <p> AYANA Komodo Resort merupakan resor pertama yang mengada di destinasi wisata Indonesia, Labuan Bajo. Tempat ini cukup mudah untuk ditempuh yakni satu jam penerbangan dari Bali. Lokasinya yakni di Pantai Waecicu, Labuan Bajo, Kabupaten Manggarai Barat, Nusa Tenggara Timur.
+    ayana komodo resort juag merupakan salah satu hotel bintang lima, dimana menjadi pilihan yang paling tepat ketika mengunjung kelabuan bajo. hotel ini memiliki fasiliteas yang sangat lengkap serta perpaduan yang sangat lengkap antara penghematan dan kenyamanan dan 
+    juga proporti ini menawarkan suasana yang romantis dengan fasilitas yang didesain untuk wisatawan yang datang menginap atau sekedar mengunjung ke hotel tersebut.</p>
+</div>
+
+<h3>berikut fasilitas-fasilitas AYANA hotel labuan bajo </h3>
+  
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>gambar 1</h3>
+      <p>keseluruhan AYANA hotel labuan bajo...</p>
+      <p>resor ini pun unik, di mana tamu mesti check-in dari lobinya yang berada di lantai 11. 
+        Sementara seluruh kamarnya berada di lantai-lantai bawahnya</p>
+      <img src="ayana 1.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 2</h3>
+      <p>view dari lantai 3 diayana hotel dipagi hari</p>
+      <p>tamu akan dimanjakan dengan pemandangan 180 derajat yang menakjubkan dari Laut Flores</p>
+      <img src="ayana 2.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 3</h3>        
+      <p>memiliki kamar dengan fasilitas yang sangat lengkap baik berupa kamar deluxe,standar,
+        maupun kamar vip dan tipe-tipe kamar lainya.intinya ayana memiliki 12 suite dan 189 kamar premium</p>
+      <img src="ayana 7.jpg"and width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 4</h3>        
+      <p>AYANA juga memeiliki beberapa restauranta di setia lantai,serta dilengkapi dengan bar masing. 
+        agar memudahkan setiap pelangggan yang menepati disetiap lantai. dan juga memiliki dua kolam renang
+        yang berada dilanti dasar satu untuk orang dewasa dan satu untuk anak-anak yang bertepatan di samping
+        restaurant lantai dasar..</p>
+      <img src="ayana 8.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 5</h3>        
+      <p>Nikmati Kemewahan Kelas Dunia serta dapatkan Keramahtamahan Indonesia di Hotel Labuan Bajo, AYANA Komodo
+         Waecicu Beach merupakan tempat peristirahatan yang sempurna yang dapat menghubungkan Anda langsung ke jantung
+         Taman Nasional Komodo. Ciptakan kenangan abadi dalam setiap sentuhan kemewahan serta fasilitas dan layanan
+         mutakhir sambil menikmati keindahan alam sekitar di hotel...</p>
+      <img src="ayana 6.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 6</h3>        
+      <p>Bagi yang ingin mengeksplor Kepulauan Komodo, AYANA Komodo Resort juga menyediakan kapal pinisidengan sembilan kamar tidur di dalamnya. Dinamai AYANA Lako’dia yang berarti “perjalanan yang aman” 
+         dalam bahasa setempat, kapal sepanjang 54 meter dan selebar 11 meter kapal ini dapat dicarter seluruhnya,atau disewa per kamarnya, untuk mengelilingi pulau-pulau di sekitarnya selama dua hingga tiga malam.</p>
+      <img src="ayana 11.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 7</h3>        
+      <p>memiliki 2 fasilitas2 kolam renang. satu untuk anak-anak dan satu untuk orang dewasa, yang luasnya sangat besar.</p>
+      <img src="kolam.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 8</h3>        
+      <p>hotel ini juga menyedikan meting room yang sangat indah dan mewah dimana letaknya berada dilantai 3,yang berkapasitas 200-250 orang</p>
+      <img src="meting.jpg" width="304" height="304"> 
+    </div>
+    <div class="col-sm-4">
+      <h3>gambar 9</h3>        
+      <p>bagai penyuka olahraga,ayana juga menyediakan fasilitas berupa gym , dimana yang berada tepat di dekat kolam renang lantai dasar(lantai 1).</p>
+      <img src="gym 2.jpg" width="304" height="304"> 
+    </div>
+  </div>
+</div>
+<h3>silahkan melihat maps dibawah</h3>  
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3946.323480922938!2d119.87228481478259!3d-8.4678910939113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2db46761a6c4d03d%3A0x3eff1256d3efd22c!2sHOTEL%20Ayana%20Komodo%20Waecicu%20Beach!5e0!3m2!1sid!2sid!4v1687195614645!5m2!1sid!2sid" width="1200" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+
+</body>
+</html>
